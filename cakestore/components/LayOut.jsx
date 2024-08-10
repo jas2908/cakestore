34a7f76377a21486322f459e2f4da43a935aa0a7
s375164 <s375164@oslomet.no>
2024-08-10 12:01:36 +0200

    layout component complete

--- cakestore/components/LayOut.jsx
+++ cakestore/components/LayOut.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
 
-const LayOut = () => {
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
   return (
-    <div>
-      
-    </div>
+      <div className="layout">
+        <Head>
+          <title>Cake Store</title>
+        </Head>
+        <header>
+          <Navbar />
+        </header>
+        <main className="main-container">
+          {children}
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
   )
 }
 
-export default LayOut
+export default Layout
